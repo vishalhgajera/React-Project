f501f17e5db3bc1086d896e6dc1a5c26779f04f5
bhargav gajera <vishgajera@gmail.com>
2022-08-02 23:33:59 +0530

    uplode all file

---
+++ src/MyComponent/MyComponent.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './MyComponent.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const MyComponent = () => (
+  
+  <div className="MyComponent">
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  </div>
+);
+
+MyComponent.propTypes = {};
+
+MyComponent.defaultProps = {};
+
+export default MyComponent;
