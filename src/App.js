8908fd6ee9fcc22306a2a268c45d4bfe1d13b5b4
bhargav gajera <vishgajera@gmail.com>
2022-08-03 00:37:46 +0530

    router link updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import React from 'react';
 import Home from './pages/home';
+import Login from "./pages/login"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-        <Home></Home> 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import FullwidthSection from '../components/FullwidthSection/FullwidthSection'
+import { Container } from '@mui/system';
+import { Button } from '@mui/material';
 
 function Login() {
   return (
     <>
       <FullwidthSection></FullwidthSection>
+     <Container>
+       <Button color='secondary'>Home page</Button>
+       <Button color='secondary'>Blogs</Button>
+       <Button color='secondary'>Contacts</Button>
+     </Container>
     </>
   );
 }
